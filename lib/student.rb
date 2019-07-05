300a38f3bf282c15003391a5544bb113e48ff313
Abagail Hudson <abagailanne@gmail.com>
2019-07-05 01:44:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,14 +86,20 @@ class Student
     end.first
   end
 
-   def self.all_in_9
-    sql = <<-SQL
-    SELECT COUNT(grade)
-    FROM students
-    WHERE grade = 9
-    SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-  end
+   def self.all_students_in_grade_9
+     sql = <<-SQL
+     SELECT COUNT(grade)
+     FROM students
+     WHERE grade = 9
+     SQL
+     DB[:conn].execute(sql).map do |row|
+       self.new_from_db(row)
+     end
+   end
+
+   def self.all_students_in_grade_X(x)
+     sql = "SELECT * FROM students WHERE grade = ?"
+     DB[:conn].execute(sql, x).map do |row|
+       self.new_from_db(row)
+     end
 end
