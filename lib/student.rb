13ac28897abccc0c961cb77ae43748c5215c3063
Abagail Hudson <abagailanne@gmail.com>
2019-07-05 01:36:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,8 +64,8 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT * 
-    FROM students 
+    SELECT *
+    FROM students
     WHERE grade < 12
     SQL
     DB[:conn].execute(sql).map do |row|
